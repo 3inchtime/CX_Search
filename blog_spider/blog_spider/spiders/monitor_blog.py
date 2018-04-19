e8abfddf85855c87a3872fd4e491ab9566feaae4
chen <3inchtime>
2018-04-19 15:31:40 +0800

    识别配置文件内容并进行初步爬取

--- blog_spider/blog_spider/spiders/monitor_blog.py
+++ blog_spider/blog_spider/spiders/monitor_blog.py
@@ -1,16 +1,40 @@
 # -*- coding: utf-8 -*-
+from urllib import parse
+
+
 import scrapy
+from scrapy.http import Request
+
 
 from blog_spider.tools.config_reader import ConfigReader
 
 
 class MonitorBlogSpider(scrapy.Spider):
+
     name = 'monitor_blog'
-    c = ConfigReader()
 
     def start_requests(self):
-        pass
+        c = ConfigReader()
+        for config in c.config_parse:
+            url = config[0]
+            meta = {
+                        'article_url': config[1],
+                        'article_title': config[2],
+                        'article_time': config[3],
+                        'next_button': config[4]
+                    }
+            yield Request(url=url, callback=self.parse, meta=meta)
 
     def parse(self, response):
+        blog_urls = response.css("{}".format(response.meta['article_url'])).extract()
+        meta = response.meta
+        for blog_url in blog_urls:
+            yield Request(url=parse.urljoin(response.url, blog_url), dont_filter=True, callback=self.parse_blog, meta=meta)
+
+    def parse_blog(self, response):
+        title = response.css("{}".format(response.meta['article_title']))
+        create_time = response.css("{}".format(response.meta['article_time']))
+        content = response.css("{}".format(response.meta['next_button']))
+        print(title, create_time, content)
         pass
 
--- blog_spider/blog_spider/tools/config_reader.py
+++ blog_spider/blog_spider/tools/config_reader.py
@@ -6,28 +6,23 @@ from blog_spider.settings import MONITOR_LIST_PATH
 
 
 class ConfigReader(object):
-	def __init__(self):
-		self.config = {}
-		self.config_parse = []
-		host_path = os.path.join(MONITOR_LIST_PATH)
-		try:
-			with open(host_path, 'r')as f:
-				self.config = yaml.load(f.read())
-		except:
-			print("解析配置文件失败")
+    def __init__(self):
+        self.config = {}
+        self.config_parse = []
+        host_path = os.path.join(MONITOR_LIST_PATH)
 
-		for key in self.config:
-			key_words = [
-								key,
-								self.config[key]['article_url'],
-								self.config[key]['article_title'],
-								self.config[key]['article_time'],
-								self.config[key]['article_category'],
-								self.config[key]['next_button']
-			]
-			self.config_parse.append(key_words)
-			print(self.config_parse)
-			self.push_data()
+        try:
+            with open(host_path, 'r')as f:
+                self.config = yaml.load(f.read())
+        except:
+            print("解析配置文件失败")
 
-	def push_data(self):
-		return self.config_parse
+        for key in self.config:
+            key_words = [
+                            key,
+                            self.config[key]['article_url'],
+                            self.config[key]['article_title'],
+                            self.config[key]['article_time'],
+                            self.config[key]['next_button']
+                          ]
+            self.config_parse.append(key_words)
